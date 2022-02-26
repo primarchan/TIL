07e9f9f5fe26a6a46885bd21ad276076ebea2b0c
Taegyun An <boltcarrier@naver.com>
2022-02-27 02:59:09 +0900

    자바스크립트 기초 강좌 3강

---
+++ javascript-basic2/#3-alert_prompt_confirm.js
@@ -0,0 +1,22 @@
+// alert : 알려줌
+// prompt : 입력 받음
+// confirm : 확인 받음
+
+// const name = prompt("이름을 입력하세요.");
+// alert("환영합니다, " + name + "님");
+
+// alert(`안녕하세요, ${name}님. 환영합니다.`);
+
+// const name = prompt("예약일을 입력해주세요.", "2022-02-");
+// alert("환영합니다, " + name + "님");
+
+// confirm은 사용자의 동작을 확인할 때 유용하게 사용됨
+// const isAdult = confirm("당신은 성인입니까?");
+// console.log(isAdult); // 확인 : true, 취소 : false
+
+/*
+단점
+1) 스크립트 일시 정지
+2) 스타일링 불가능, 브라우저 마다 스타일이 다름
+*/
+
