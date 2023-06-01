4e050ff99db5fcc7b576f243ef90b4aaf13f92ee
An Tae Gyun <boltcarrier@naver.com>
2023-06-02 02:18:15 +0900

    [JPA] 3.2강 Entity Mapping(4)

--- jpa/springboot-jpa-practice/src/main/java/com/example/springbootjpapractice/entity/Customer.java
+++ jpa/springboot-jpa-practice/src/main/java/com/example/springbootjpapractice/entity/Customer.java
@@ -12,11 +12,13 @@ import javax.persistence.*;
 @ToString
 @NoArgsConstructor
 @SequenceGenerator(name = "customer_generator", sequenceName = "customer_seq", initialValue = 1, allocationSize = 50)
+@TableGenerator(name = "id_generator", table = "customer_id",pkColumnName = "id_name", pkColumnValue = "customer_id", valueColumnName = "next_value", initialValue = 0, allocationSize = 1)
 public class Customer {
 
     @Id
     // @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_generator")
+    // @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_generator")
+    @GeneratedValue(strategy = GenerationType.TABLE, generator = "id_generator")
     private String id;
 
     private String name;
